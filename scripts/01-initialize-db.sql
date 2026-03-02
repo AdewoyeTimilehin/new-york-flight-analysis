40ca76d58785f34e92b470f367642ce8d0f8b482
eustudyassist <eustudyassist@gmail.com>
2026-03-02 05:22:12 -0800

    chore: clean up code, and remove repetitive blocks

--- scripts/01-initialize-db.sql
+++ scripts/01-initialize-db.sql
@@ -36,86 +36,78 @@ WITH (FORMAT CSV, HEADER, NULL 'NA');
 SELECT *
 FROM airports;
 
-
-
-DROP TABLE planes;
 CREATE TABLE planes(
-          sn SMALLINT,
-          tailnum CHAR(6),
-   		  year INT,
-          type VARCHAR(50),
-          manufacturer VARCHAR(50),
-          model VARCHAR(50),
-		  engines CHAR(1),
-		  seats SMALLINT,
-		  speed SMALLINT,
-		  engine VARCHAR(50)
+	sn SMALLINT,
+	tailnum CHAR(6),
+	year INT,
+	type VARCHAR(50),
+	manufacturer VARCHAR(50),
+	model VARCHAR(50),
+	engines CHAR(1),
+	seats SMALLINT,
+	speed SMALLINT,
+	engine VARCHAR(50)
 );
 
 COPY planes
-FROM 'C:\Users\HomePC\Documents\SQL\new-york-flight-analysis\data\planes.csv'
+FROM 'C:\Users\eustudyassist\Documents\new-york-flight-analysis\data\planes.csv'
 WITH (FORMAT CSV, HEADER, NULL 'NA');
 
 SELECT *
 FROM planes;
 
-DROP TABLE weather;
-
 CREATE TABLE weather(
-		sn SMALLINT,
-		origin CHAR(3),
-		year INT,
-		month SMALLINT,
-		day INT,
-		hour INT,
-		temp REAL,
-		dewp REAL,
-		humid REAL,
-		wind_dir INT,
-		wind_speed NUMERIC(10, 7),
-		wind_gust NUMERIC(15, 12),
-		precip REAL,
-		pressure REAL,
-		visib REAL,
-		time_hour TIMESTAMP
+	sn SMALLINT,
+	origin CHAR(3),
+	year INT,
+	month SMALLINT,
+	day INT,
+	hour INT,
+	temp REAL,
+	dewp REAL,
+	humid REAL,
+	wind_dir INT,
+	wind_speed NUMERIC(10, 7),
+	wind_gust NUMERIC(15, 12),
+	precip REAL,
+	pressure REAL,
+	visib REAL,
+	time_hour TIMESTAMP
 );
 
 COPY weather
-FROM 'C:\Users\HomePC\Documents\SQL\new-york-flight-analysis\data\weather.csv'
+FROM 'C:\Users\eustudyassist\Documents\new-york-flight-analysis\data\weather.csv'
 WITH (FORMAT CSV, HEADER, NULL 'NA');
 
 SELECT *
 FROM weather;
 
-
-DROP TABLE flights;
-
 CREATE TABLE flights(
-		sn INT,
-		year INT,
-		month SMALLINT,
-		day INT,
-		dep_time INT,
-		sched_dep_time INT,
-		dep_delay INT,
-		arr_time INT,
-		sched_arr_time INT,
-		arr_delay INT,
-		carrier CHAR(2),
-		flight INT,
-		tailnum CHAR(6),
-		origin CHAR(3),
-		dest CHAR(3),
-		air_time INT,
-		distance INT,
-		hour INT,
-		minute INT,
-		time_hour TIMESTAMP
+	sn INT,
+	year INT,
+	month SMALLINT,
+	day INT,
+	dep_time INT,
+	sched_dep_time INT,
+	dep_delay INT,
+	arr_time INT,
+	sched_arr_time INT,
+	arr_delay INT,
+	carrier CHAR(2),
+	flight INT,
+	tailnum CHAR(6),
+	origin CHAR(3),
+	dest CHAR(3),
+	air_time INT,
+	distance INT,
+	hour INT,
+	minute INT,
+	time_hour TIMESTAMP
 );
 
 
 COPY flights
-FROM 'C:\Users\HomePC\Documents\SQL\new-york-flight-analysis\data\flights.csv'
+FROM 'C:\Users\eustudyassist\Documents\new-york-flight-analysis\data\flights.csv'
 WITH (FORMAT CSV, HEADER, NULL 'NA');
 
 SELECT *
